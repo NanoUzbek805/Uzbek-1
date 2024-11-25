87dbe588ccf362f021b177bda082f38c20d5e6f4
NanoUzbek805 <niko.miro.ua@gmail.com>
2024-11-25 15:11:53 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+
+$(document).ready(function(){
+  $('.carousele').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    dots: true, // Відображення крапок для навігації
+    arrows: true, // Відображення стрілок
+  });
+});
+
+function scrollToSection(id) {
+  document.getElementById(id).scrollIntoView({
+      behavior: 'smooth'
+  });
+}
